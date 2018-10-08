235b662845fba450e5a6ff80f445b76304d5762e
Felipe Soares <altran.felipe@grupofleury.com.br>
2018-10-08 10:18:07 -0300

    Rabbit MQ implementation.

---
+++ Code/CashFlow.Model/Repository/IQueueManager.cs
@@ -0,0 +1,17 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CashFlow.Domain.Repository
+{
+    public interface IQueueManager
+    {
+
+        ConnectionFactory GetConnectionFactory();
+        IConnection GetConnection(ConnectionFactory connectionFactory);
+        QueueDeclareOk CreateQueue(Func<IModel, QueueDeclareOk> queueDeclare, IConnection connection);
+        bool TryWriteMessageOnQueue(string message, string queueName, IConnection connection);
+        string RetrieveSingleMessage(string queueName, IConnection connection);
+    }
+}
