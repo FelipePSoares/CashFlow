3cc8eadf626dbc8ad89cd79d70257e507eef1bc7
Felipe Soares <altran.felipe@grupofleury.com.br>
2018-10-08 08:29:17 -0300

    Project base created

---
+++ Code/CashFlow.Api/Controllers/CashFlowController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace CashFlow.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CashFlowController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult CashFlow()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("received")]
+        public IActionResult Received()
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPost("payment")]
+        public IActionResult Payment()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
